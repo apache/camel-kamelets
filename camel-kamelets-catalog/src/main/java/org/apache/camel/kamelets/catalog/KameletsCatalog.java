a3e1c825f1047018a10bd222186e45bca4ffd2b1
Andrea Cosentino <ancosen@gmail.com>
2021-04-27 15:23:49 +0200

    Kamelets Catalog: First implementation

---
+++ camel-kamelets-catalog/src/main/java/org/apache/camel/kamelets/catalog/KameletsCatalog.java
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.kamelets.catalog;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import io.fabric8.camelk.v1alpha1.Kamelet;
+import org.apache.commons.io.Charsets;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class KameletsCatalog {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KameletsCatalog.class);
+    private static final String KAMELETS_DIR = "kamelets";
+    private static final String KAMELETS_FILE_SUFFIX = ".kamelet.yaml";
+    private static ObjectMapper mapper = new ObjectMapper(new YAMLFactory()).configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    private Map<String, Kamelet> kameletModels = new HashMap<>();
+
+    public KameletsCatalog() throws IOException {
+        initCatalog();
+    }
+
+    private void initCatalog() throws IOException {
+        List<String> files = IOUtils.readLines(KameletsCatalog.class.getClassLoader().getResourceAsStream(KAMELETS_DIR), Charsets.UTF_8);
+        for (String fileName:
+             files) {
+            Kamelet kamelet = mapper.readValue(KameletsCatalog.class.getClassLoader().getResourceAsStream(KAMELETS_DIR + File.separator + fileName), Kamelet.class);
+            kameletModels.put(sanitizeFileName(fileName), kamelet);
+        }
+    }
+
+    private String sanitizeFileName(String fileName) {
+        int index = fileName.lastIndexOf(KAMELETS_FILE_SUFFIX);
+        if (index > 0) {
+            fileName = fileName.substring(0, index);
+        }
+        return fileName;
+    }
+
+    public Map<String, Kamelet> getKamelets() {
+        return kameletModels;
+    }
+
+    public List<Kamelet> getKameletsByName(String name) {
+        List<Kamelet> collect = kameletModels.entrySet().stream()
+                .filter(x -> x.getKey().contains(name))
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    public List<Kamelet> getKameletsByType(String type) {
+        List<Kamelet> collect = kameletModels.entrySet().stream()
+                .filter(x -> x.getValue().getMetadata().getLabels().get("camel.apache.org/kamelet.type").contains(type))
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+        return collect;
+    }
+}
