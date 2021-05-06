a5e0a917ef0ae725287e3ef3df37b2345fe4ce38
Andrea Cosentino <ancosen@gmail.com>
2021-05-06 13:05:00 +0200

    Attempt to use JSONNode as base building block in data manipulation

--- library/camel-kamelets-utils/src/main/java/org/apache/camel/kamelets/utils/transform/InsertField.java
+++ library/camel-kamelets-utils/src/main/java/org/apache/camel/kamelets/utils/transform/InsertField.java
@@ -19,21 +19,17 @@ package org.apache.camel.kamelets.utils.transform;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.camel.Exchange;
 import org.apache.camel.ExchangeProperty;
 import org.apache.camel.InvalidPayloadException;
 
 public class InsertField {
 
-    public Map<?, ?> process(@ExchangeProperty("field") String field, @ExchangeProperty("value") String value, Exchange ex) throws InvalidPayloadException {
-        Map<Object, Object> body = ex.getMessage().getBody(Map.class);
-        if (body == null) {
-            String val = ex.getMessage().getMandatoryBody(String.class);
-            body = new HashMap<>();
-            // TODO: make this configurable
-            body.put("content", val);
-        }
-        body.put(field, value);
+    public JsonNode process(@ExchangeProperty("field") String field, @ExchangeProperty("value") String value, Exchange ex) throws InvalidPayloadException {
+        JsonNode body = ex.getMessage().getBody(JsonNode.class);
+        ((ObjectNode) body).put(field, value);
         return body;
     }
 
