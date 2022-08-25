b02a411a23a59e6388a33d5ce3416bfd6cc8d388
Andrea Cosentino <ancosen@gmail.com>
2022-08-25 15:31:20 +0200

    Added a Mask Field test for Action

---
+++ library/camel-kamelets-utils/src/test/java/org/apache/camel/kamelets/utils/transform/MaskFieldTest.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.kamelets.utils.transform;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.camel.Exchange;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.support.DefaultExchange;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MaskFieldTest {
+
+    private DefaultCamelContext camelContext;
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    private MaskField processor;
+
+    private final String baseJson = "{" + "\n" +
+            "  \"name\" : \"Rajesh Koothrappali\"" + "\n" +
+            "}";
+
+    @BeforeEach
+    void setup() {
+        camelContext = new DefaultCamelContext();
+        processor = new MaskField();
+    }
+
+    @Test
+    void shouldMaskFieldFromJsonNode() throws Exception {
+        Exchange exchange = new DefaultExchange(camelContext);
+
+        exchange.getMessage().setBody(mapper.readTree(baseJson));
+
+        JsonNode s = processor.process("name", "xxxx", exchange);
+        Assertions.assertEquals("\"xxxx\"" , s.get("name").toString());
+    }
+
+}
