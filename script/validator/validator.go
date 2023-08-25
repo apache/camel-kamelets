92320f96ccb38bb8ebf3a603b7ba774ec4a9e41f
Andrea Cosentino <ancosen@gmail.com>
2023-08-25 18:28:11 +0200

    Added Opensearch Search Source Kamelet (#1594)
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- script/validator/validator.go
+++ script/validator/validator.go
@@ -402,7 +402,7 @@ func listKamelets(dir string) []KameletInfo {
 
 func verifyUsedParams(kamelets []KameletInfo) (errors []error) {
 	for _, k := range kamelets {
-	        if (k.FileName != "../../kamelets/azure-storage-blob-source.kamelet.yaml" && k.FileName != "../../kamelets/aws-s3-cdc-source.kamelet.yaml"  && k.FileName != "../../kamelets/set-kafka-key-action.kamelet.yaml"  && k.FileName != "../../kamelets/azure-storage-blob-cdc-source.kamelet.yaml" && k.FileName != "../../kamelets/google-storage-cdc-source.kamelet.yaml" && k.FileName != "../../kamelets/elasticsearch-search-source.kamelet.yaml") {
+	        if (k.FileName != "../../kamelets/azure-storage-blob-source.kamelet.yaml" && k.FileName != "../../kamelets/aws-s3-cdc-source.kamelet.yaml"  && k.FileName != "../../kamelets/set-kafka-key-action.kamelet.yaml"  && k.FileName != "../../kamelets/azure-storage-blob-cdc-source.kamelet.yaml" && k.FileName != "../../kamelets/google-storage-cdc-source.kamelet.yaml" && k.FileName != "../../kamelets/elasticsearch-search-source.kamelet.yaml" && k.FileName != "../../kamelets/opensearch-search-source.kamelet.yaml") {
 		used := getUsedParams(k.Kamelet)
 		declared := getDeclaredParams(k.Kamelet)
 		for p := range used {
