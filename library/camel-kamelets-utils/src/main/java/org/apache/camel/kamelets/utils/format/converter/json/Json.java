11f723ba70d7db1efe3d74d39586559e94dfd638
Christoph Deppisch <cdeppisch@redhat.com>
2023-09-19 18:18:19 +0200

    fix(#1653): Add Google Sheets Sink Kamelet
    - Add google-sheets-sink Kamelet that updates/appends range cell data to a spreadsheet
    - Add GoogleSheetsJsonStructDataType that supports transforming generic JsonNode structs from/to ValueRange object
    - Add generic json struct data type support to google-sheets-source Kamelet to transform Google Sheets ValueRange object to pure row/column values
    - Adjust google-sheets-source Kamelet to support custom columnNames and majorDimension setting
    - Expose data type information on google-sheets-source and -sink Kamelets

--- library/camel-kamelets-utils/src/main/java/org/apache/camel/kamelets/utils/format/converter/json/Json.java
+++ library/camel-kamelets-utils/src/main/java/org/apache/camel/kamelets/utils/format/converter/json/Json.java
@@ -17,7 +17,15 @@
 
 package org.apache.camel.kamelets.utils.format.converter.json;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.TextNode;
+import org.apache.camel.util.StringHelper;
 
 public final class Json {
 
@@ -25,5 +33,75 @@ public final class Json {
 
     private Json() {
         // prevent instantiation of utility class
+    }
+
+    /**
+     * Checks given value to be a Json array of object representation.
+     * @param value
+     * @return
+     */
+    public static boolean isJson(String value) {
+        if (value == null) {
+            return false;
+        }
+
+        return isJsonObject(value) || isJsonArray(value);
+    }
+
+    /**
+     * Checks given value could be JSON object string.
+     * @param value
+     * @return
+     */
+    public static boolean isJsonObject(String value) {
+        if (value == null || value.isEmpty() || value.isBlank()) {
+            return false;
+        }
+
+        final String trimmed = value.trim();
+
+        return trimmed.charAt(0) == '{' && trimmed.charAt(trimmed.length() - 1) == '}';
+    }
+
+    /**
+     * Checks given value could be JSON array string.
+     * @param value
+     * @return
+     */
+    public static boolean isJsonArray(String value) {
+        if (value == null || value.isEmpty() || value.isBlank()) {
+            return false;
+        }
+
+        final String trimmed = value.trim();
+
+        return trimmed.charAt(0) == '[' && trimmed.charAt(trimmed.length() - 1) == ']';
+    }
+
+    /**
+     * Converts array json node to a list of json object strings. Used when splitting a
+     * json array with split EIP.
+     * @param json
+     * @return
+     * @throws JsonProcessingException
+     */
+    public static List<String> arrayToJsonBeans(JsonNode json) throws JsonProcessingException {
+        List<String> jsonBeans = new ArrayList<>();
+
+        if (json.isArray()) {
+            Iterator<JsonNode> it = json.elements();
+            while (it.hasNext()) {
+                Object item = it.next();
+                if (item instanceof TextNode) {
+                    jsonBeans.add(StringHelper.removeLeadingAndEndingQuotes(((TextNode) item).asText()));
+                } else {
+                    jsonBeans.add(MAPPER.writeValueAsString(item));
+                }
+            }
+
+            return jsonBeans;
+        }
+
+        return jsonBeans;
     }
 }
