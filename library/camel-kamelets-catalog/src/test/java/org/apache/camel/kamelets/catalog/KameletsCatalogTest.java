4e332dbbde69106ec64133c0ee62d082ef08883e
Andrea Cosentino <ancosen@gmail.com>
2023-11-21 13:18:26 +0100

    Create Kamelets for AWS Timestream
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- library/camel-kamelets-catalog/src/test/java/org/apache/camel/kamelets/catalog/KameletsCatalogTest.java
+++ library/camel-kamelets-catalog/src/test/java/org/apache/camel/kamelets/catalog/KameletsCatalogTest.java
@@ -103,7 +103,7 @@ public class KameletsCatalogTest {
     void testGetKameletsByNamespace() throws Exception {
         List<Kamelet> c = catalog.getKameletsByNamespace("AWS");
         assertFalse(c.isEmpty());
-        assertEquals(25, c.size());
+        assertEquals(26, c.size());
         c = catalog.getKameletsByGroups("Not-existing-group");
         assertTrue(c.isEmpty());
     }
