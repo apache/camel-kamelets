bd74f5a90bb40220816e13bb3c06111499ddef63
Christoph Deppisch <cdeppisch@redhat.com>
2025-01-29 12:15:53 +0100

    chore: Improve integration tests (#2370)
    * chore: Remove obsolete workaround
    
    * chore: Improve stability of integration tests
    
    - Avoid to use the very same Http server instance for multiple tests
    - Avoid racing conditions where Http requests are sent by the Camel JBang process after the test has finished
    - Introduce individual Http server instances for the tests (jiraServer, slackServer, petstoreServer, httpServer)
    - Better separation of tests due to individual server ports

--- tests/camel-kamelets-itest/src/test/java/EndpointAutoConfiguration.java
+++ tests/camel-kamelets-itest/src/test/java/EndpointAutoConfiguration.java
@@ -17,22 +17,52 @@
 
 import org.citrusframework.annotations.CitrusConfiguration;
 import org.citrusframework.container.SequenceAfterTest;
+import org.citrusframework.container.SequenceBeforeTest;
 import org.citrusframework.http.server.HttpServer;
 import org.citrusframework.spi.BindToRegistry;
+import org.citrusframework.util.SocketUtils;
 import org.springframework.http.HttpStatus;
 
+import static org.citrusframework.actions.CreateVariablesAction.Builder.createVariables;
 import static org.citrusframework.actions.PurgeEndpointAction.Builder.purgeEndpoints;
 import static org.citrusframework.http.endpoint.builder.HttpEndpoints.http;
-import static org.citrusframework.jbang.actions.JBangAction.Builder.jbang;
 
 @CitrusConfiguration
 public class EndpointAutoConfiguration {
 
+    private static final long SERVER_TIMEOUT = 60000L;
+
+    private static final int HTTP_SERVER_PORT = SocketUtils.findAvailableTcpPort(8801);
+    private static final int SLACK_SERVER_PORT = SocketUtils.findAvailableTcpPort(8802);
+    private static final int PETSTORE_SERVER_PORT = SocketUtils.findAvailableTcpPort(8803);
+    private static final int JIRA_SERVER_PORT = SocketUtils.findAvailableTcpPort(8804);
+
     private final HttpServer httpServer = http()
             .server()
-            .port(8081)
+            .port(HTTP_SERVER_PORT)
+            .timeout(SERVER_TIMEOUT)
+            .autoStart(true)
+            .build();
+
+    private final HttpServer slackServer = http()
+            .server()
+            .port(SLACK_SERVER_PORT)
+            .timeout(SERVER_TIMEOUT)
+            .autoStart(true)
+            .build();
+
+    private final HttpServer petstoreServer = http()
+            .server()
+            .port(PETSTORE_SERVER_PORT)
+            .timeout(SERVER_TIMEOUT)
             .defaultStatus(HttpStatus.CREATED)
-            .timeout(60000L)
+            .autoStart(true)
+            .build();
+
+    private final HttpServer jiraServer = http()
+            .server()
+            .port(JIRA_SERVER_PORT)
+            .timeout(SERVER_TIMEOUT)
             .autoStart(true)
             .build();
 
@@ -42,13 +72,44 @@ public class EndpointAutoConfiguration {
     }
 
     @BindToRegistry
+    public HttpServer slackServer() {
+        return slackServer;
+    }
+
+    @BindToRegistry
+    public HttpServer petstoreServer() {
+        return petstoreServer;
+    }
+
+    @BindToRegistry
+    public HttpServer jiraServer() {
+        return jiraServer;
+    }
+
+    @BindToRegistry
+    public SequenceBeforeTest beforeTest() {
+        return SequenceBeforeTest.Builder.beforeTest()
+                .actions(
+                    // Set server ports as test variables
+                    createVariables()
+                            .variable("http.server.port", String.valueOf(HTTP_SERVER_PORT))
+                            .variable("slack.server.port", String.valueOf(SLACK_SERVER_PORT))
+                            .variable("petstore.server.port", String.valueOf(PETSTORE_SERVER_PORT))
+                            .variable("jira.server.port", String.valueOf(JIRA_SERVER_PORT))
+                )
+                .build();
+    }
+
+    @BindToRegistry
     public SequenceAfterTest afterTest() {
         return SequenceAfterTest.Builder.afterTest()
                 .actions(
-                    // Workaround to stop all Camel JBang integrations after test - remove when Citrus 4.5.2 is released
-                    jbang().app("camel@apache/camel").command("stop"),
                     // Auto purge Http server endpoint
-                    purgeEndpoints().endpoint(httpServer)
+                    purgeEndpoints()
+                            .endpoint(httpServer)
+                            .endpoint(slackServer)
+                            .endpoint(petstoreServer)
+                            .endpoint(jiraServer)
                 )
                 .build();
     }
